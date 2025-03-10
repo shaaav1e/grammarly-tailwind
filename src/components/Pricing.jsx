6eb89774d8ba6241fb3c90d4ac09240697f22514
shaaav1e <shaaavie@gmail.com>
2025-03-10 17:04:33 +0500

    Pricing done- need to add gradient color on border

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -10,21 +10,22 @@ const Pricing = () => {
             {/* Upper Part */}
             <div className="p-6 mx-3 mt-3 mb-3 rounded-sm bg-white flex-1 flex flex-col">
               <div className="">
-                <div className="text-center uppercase md:text-left">
+                <div className="text-center uppercase md:text-left text-sm">
                   For individuals
                 </div>
                 <h2 className="mt-10 font-serif text-5xl text-center md:text-left">
                   Free
                 </h2>
                 <p className="mt-10 text-center md:text-left">
-                  Get peace of mind with writing that's mistake-free.
+                  Get peace of mind with writing that's mistake-free and
+                  consistent.
                 </p>
                 <p className="mt-10 font-semibold text-5xl text-center md:text-left">
                   $0<span className="text-sm">USD</span>
                 </p>
               </div>
 
-              <div className="mt-25">
+              <div className="lg:mt-25 mt-3">
                 <button className="btn-pricing block mx-auto w-full whitespace-nowrap">
                   Create Account
                 </button>
@@ -51,8 +52,8 @@ const Pricing = () => {
           <div className="rounded-xl text-black bg-green-200 lg:w-1/3 order-first lg:order-none flex flex-col">
             {/* Upper Part */}
             <div className="p-6 mx-3 mt-3 mb-3 rounded-sm bg-white flex-1 flex flex-col">
-              <div className="flex-1">
-                <div className="text-center uppercase md:text-left">
+              <div className="">
+                <div className="text-center uppercase md:text-left text-sm">
                   For individuals or teams
                 </div>
                 <h2 className="mt-10 font-serif text-5xl text-center md:text-left">
@@ -64,13 +65,18 @@ const Pricing = () => {
                 <p className="mt-10 font-semibold text-5xl text-center md:text-left">
                   $12<span className="text-sm">USD</span>
                 </p>
-                <p className="mt-2 text-sm text-center md:text-left">
-                  /member/month/billed annually
+                <p className="text-sm">
+                  / member / month, billed annually
+                  <span>
+                    <br />
+                    <span className="font-bold">$30</span> when billed monthly
+                    <br />
+                  </span>
                 </p>
               </div>
 
-              <div className="">
-                <button className="btn-pricing block mx-auto w-full whitespace-nowrap">
+              <div className="lg:mt-15 mt-4">
+                <button className="btn block mx-auto w-full whitespace-nowrap">
                   Get Started
                 </button>
               </div>
@@ -78,7 +84,7 @@ const Pricing = () => {
               <div className="mt-10">
                 <div className="flex items-center mt-4 gap-2">
                   <i className="ri-checkbox-circle-line"></i>
-                  <p className="font-bold">Everything included in free</p>
+                  <p className="font-bold">Everything included in Free</p>
                 </div>
                 <div className="flex items-center mt-4 gap-2">
                   <i className="ri-checkbox-circle-line"></i>
@@ -94,18 +100,17 @@ const Pricing = () => {
                 </div>
                 <div className="flex items-center mt-4 gap-2">
                   <i className="ri-checkbox-circle-line"></i>
-                  <p>Generate text with AI prompts</p>
+                  <p>Generate text with 2,000 AI prompts</p>
                 </div>
               </div>
             </div>
           </div>
-
           {/* Col 3 - Enterprise */}
           <div className="rounded-xl text-black bg-green-200 lg:w-1/3 flex flex-col">
             {/* Upper Part */}
             <div className="p-6 mx-3 mt-3 mb-3 rounded-sm bg-white flex-1 flex flex-col">
-              <div className="flex-1">
-                <div className="text-center uppercase md:text-left">
+              <div className="">
+                <div className="text-center uppercase md:text-left text-sm">
                   For larger organizations
                 </div>
                 <h2 className="mt-10 font-serif text-5xl text-center md:text-left">
@@ -114,9 +119,12 @@ const Pricing = () => {
                 <p className="mt-10 text-center md:text-left">
                   Drive results across your entire organization with trusted AI.
                 </p>
+                <p className="mt-10 font-semibold text-5xl text-center md:text-left invisible">
+                  hidden
+                </p>
               </div>
 
-              <div className="mt-48">
+              <div className="lg:mt-25 mt-3">
                 <button className="btn-pricing block mx-auto w-full whitespace-nowrap">
                   Contact Sales
                 </button>
